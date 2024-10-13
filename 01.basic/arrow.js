fe5590d21034b5fcad93eb001310f63f0acd1e0c
Md. Golam Rabbi <156872494+mad-robot221@users.noreply.github.com>
2024-10-13 03:15:28 +0000

    arrowFunction

---
+++ 01.basic/arrow.js
@@ -0,0 +1,24 @@
+// const obj = { name: "Rabbi",
+//     price: 234,
+
+// greeting : function(name){
+//     console.log(`${this.name} is the best`)
+//     console.log(this);
+
+//     }}
+
+// obj.greeting()
+// obj.name = "sam"
+// obj.greeting()
+
+// function chai() {
+//     console.log(this);
+    
+// }
+
+// chai()
+
+const addTwo = (num1 , num2) => num1 + num2 
+//implecite return
+
+ console.log(addTwo(4 ,5))
